7b80bd64e646ac6972fe6e4635e0b875a71a6dc7
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-07 00:31:05 +0800

    成绩更新（网页录入） 2026-09-06

--- js/data.js
+++ js/data.js
@@ -3,5 +3,25 @@
  * 字段说明见 js/data.js 顶部注释。
  * ============================================================ */
 window.MHRS_RECORDS = [
-
+  {
+    "id": "rmtq13igo",
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX9",
+    "rule": "sanyou",
+    "monsterId": "m57",
+    "weaponId": "db",
+    "timeMs": 214400,
+    "author": "BFeather7",
+    "date": "2026-01-03",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1tMiqBuET8",
+        "title": ""
+      }
+    ],
+    "platform": "steam",
+    "note": ""
+  }
 ];
