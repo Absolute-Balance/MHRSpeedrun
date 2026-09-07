9a3d2afb5578e8d3ad1e168e1f2b8e891e1831d7
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-07 16:40:31 +0800

    成绩更新（网页录入） 2026-09-07

--- js/data.js
+++ js/data.js
@@ -1581,27 +1581,6 @@ window.MHRS_RECORDS = [
   {
     "questType": "special",
     "quest": null,
-    "exStar": "EX9",
-    "rule": "ta",
-    "monsterId": "m56",
-    "weaponId": "db",
-    "timeMs": 312350,
-    "author": "超好骗的雅各布",
-    "date": "2026-09-07",
-    "videos": [
-      {
-        "site": "bilibili",
-        "url": "https://www.bilibili.com/video/BV1CV66YoE5K",
-        "title": "【MHRS】免于生庆 双剑 lv300 怪异克服天廻龙 TA规则 5′12″35"
-      }
-    ],
-    "platform": "steam",
-    "note": "",
-    "id": "smtqyhaxf"
-  },
-  {
-    "questType": "special",
-    "quest": null,
     "exStar": "EX3",
     "rule": "sanyou",
     "monsterId": "m18",
