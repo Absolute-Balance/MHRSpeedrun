d21874769f6183cd1a8a869acb23bb62f6486c6e
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-07 23:56:19 +0800

    审核通过：投稿发布 2026-09-07

--- js/data.js
+++ js/data.js
@@ -3467,5 +3467,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "smtrf9irp"
+  },
+  {
+    "questType": "anomaly300",
+    "quest": null,
+    "exStar": "EX1",
+    "rule": "ta",
+    "monsterId": "m03",
+    "weaponId": "lance",
+    "timeMs": 174420,
+    "author": "这里是庸人",
+    "date": "2026-02-13",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1YYcGzGEzi",
+        "title": "【MHRS/生庆】怪异LV.300鸟龙三连星合集 长枪 TA"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smtrfapk6"
   }
 ];
