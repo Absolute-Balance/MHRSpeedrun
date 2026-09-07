6dfc9f205012a8df9cce8376f9be3e4fdf6b8ea6
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-07 21:51:30 +0800

    成绩更新（网页录入） 2026-09-07

--- js/data.js
+++ js/data.js
@@ -2438,5 +2438,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmtr9eaj0"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX7",
+    "rule": "sanyou",
+    "monsterId": "m47",
+    "weaponId": "db",
+    "timeMs": 365750,
+    "author": "真名-玛亚特加那芙",
+    "date": "2026-08-23",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1pk8b6wEBR",
+        "title": "MHRS双刀特别探究银火龙6'05''75三无"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtratqxs"
   }
 ];
