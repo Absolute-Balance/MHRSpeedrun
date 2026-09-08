17e766d3cff1da7c139872d588322b3516530679
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-08 15:58:23 +0800

    审核通过：投稿发布 2026-09-08

--- js/data.js
+++ js/data.js
@@ -4706,5 +4706,26 @@ window.MHRS_RECORDS = [
     "platform": "switch",
     "note": "",
     "id": "rmtsdl8c9"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX3",
+    "rule": "ta",
+    "monsterId": "m19",
+    "weaponId": "sa",
+    "timeMs": 357500,
+    "author": "時渡",
+    "date": "2025-12-27",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1d9BzBAECQ",
+        "title": "【MHRS】斩斧 特别探究：绯天狗兽 5分57秒 TA 防具无炼化"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smtsdnxzw"
   }
 ];
