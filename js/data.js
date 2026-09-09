96f72f21ae92610cecabde6c9ed27bd589517fc5
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-09 09:46:55 +0800

    成绩更新（网页录入） 2026-09-09

--- js/data.js
+++ js/data.js
@@ -5970,27 +5970,6 @@ window.MHRS_RECORDS = [
   {
     "questType": "special",
     "quest": null,
-    "exStar": "EX7",
-    "rule": "sanyou",
-    "monsterId": "m50",
-    "weaponId": "hammer",
-    "timeMs": 449180,
-    "author": "Marshmollow",
-    "date": "2026-07-17",
-    "videos": [
-      {
-        "site": "bilibili",
-        "url": "https://www.bilibili.com/video/BV1N7KL6BEi9",
-        "title": "大锤 超特激昂金狮子 7分29秒18  三无规则"
-      }
-    ],
-    "platform": "steam",
-    "note": "",
-    "id": "smtte0wkb"
-  },
-  {
-    "questType": "special",
-    "quest": null,
     "exStar": "EX2",
     "rule": "sanyou",
     "monsterId": "m12",
