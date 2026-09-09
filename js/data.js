c9434a0bc1ce3a741809081c525ae98e31456f3d
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-09 11:41:09 +0800

    成绩更新（网页录入） 2026-09-09

--- js/data.js
+++ js/data.js
@@ -366,27 +366,6 @@ window.MHRS_RECORDS = [
     "exStar": "EX9",
     "rule": "sanyou",
     "monsterId": "m56",
-    "weaponId": "hammer",
-    "timeMs": 478990,
-    "author": "Marshmollow",
-    "date": "2026-08-23",
-    "videos": [
-      {
-        "site": "bilibili",
-        "url": "https://www.bilibili.com/video/BV1MA8x6wEFc",
-        "title": "大锤 特别探究·天廻龙 7分58秒94 三无规则"
-      }
-    ],
-    "platform": "steam",
-    "note": "",
-    "id": "rmtqobjah"
-  },
-  {
-    "questType": "special",
-    "quest": null,
-    "exStar": "EX9",
-    "rule": "sanyou",
-    "monsterId": "m56",
     "weaponId": "hh",
     "timeMs": 419990,
     "author": "病房里的五香蛋丶",
