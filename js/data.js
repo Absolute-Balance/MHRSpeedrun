0b985a5736ed388c7e74ff294ec0d80f3d71e373
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-10 11:35:29 +0800

    成绩更新（网页录入） 2026-09-10

--- js/data.js
+++ js/data.js
@@ -6953,5 +6953,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmtuz41d0"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX2",
+    "rule": "ta",
+    "monsterId": "m09",
+    "weaponId": "sns",
+    "timeMs": 539640,
+    "author": "yiopk",
+    "date": "2026-07-25",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1zygq6PExC",
+        "title": "【MHRS】片手 特别探究：天狗兽 8'59''64 TA规则"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtuz5inm"
   }
 ];
