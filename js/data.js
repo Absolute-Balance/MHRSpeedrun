a2815ad5a2c144f35d91b106470eba995fa4e6c3
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-10 12:07:12 +0800

    成绩更新（网页录入） 2026-09-10

--- js/data.js
+++ js/data.js
@@ -7205,5 +7205,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmtv0905n"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX2",
+    "rule": "ta",
+    "monsterId": "m12",
+    "weaponId": "sns",
+    "timeMs": 547730,
+    "author": "yiopk",
+    "date": "2026-06-29",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1xJTG6YEYx",
+        "title": "【MHRS】片手 特别探究：土砂龙 9'07''73 TA规则"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtv0aaya"
   }
 ];
