4bd02407503e0499aa997847d00a5c65c9f28f09
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-11 10:47:02 +0800

    成绩更新（网页录入） 2026-09-11

--- js/data.js
+++ js/data.js
@@ -7541,5 +7541,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmtwcsxfy"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX5",
+    "rule": "sanyou",
+    "monsterId": "m39",
+    "weaponId": "hammer",
+    "timeMs": 393990,
+    "author": "Marshmollow",
+    "date": "2026-09-10",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV19PY86oENS",
+        "title": "【崛起 杂物】"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtwcv0dr"
   }
 ];
