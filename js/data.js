671088e9ced3d0c1838f77e74b2728bb235e63f7
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-11 10:45:26 +0800

    成绩更新（网页录入） 2026-09-11

--- js/data.js
+++ js/data.js
@@ -7520,5 +7520,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "smtvaoweq"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX5",
+    "rule": "sanyou",
+    "monsterId": "m39",
+    "weaponId": "sns",
+    "timeMs": 398990,
+    "author": "Marshmollow",
+    "date": "2026-09-10",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1dFY868EMM",
+        "title": "片手剑 特别探究·轰龙 6分38秒 三无规则"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtwcsxfy"
   }
 ];
