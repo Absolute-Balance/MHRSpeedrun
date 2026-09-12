27fa77817d655b8b7ee73c81485f2c8d6783edb2
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-12 10:33:52 +0800

    审核通过：投稿发布 2026-09-12

--- js/data.js
+++ js/data.js
@@ -7982,5 +7982,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "smtxro6cg"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX3",
+    "rule": "sanyou",
+    "monsterId": "m20",
+    "weaponId": "cb",
+    "timeMs": 354160,
+    "author": "恶魔的儿子",
+    "date": "2026-08-12",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1bngG6CEmS",
+        "title": "【MHRS】盾斧特别探究雌火龙5分54秒"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smtxru0hq"
   }
 ];
