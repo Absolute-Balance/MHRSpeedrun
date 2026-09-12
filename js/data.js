6a423726d3acaf883741b40180ce0a61e649b37d
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-12 16:33:09 +0800

    审核通过：投稿发布 2026-09-12

--- js/data.js
+++ js/data.js
@@ -9683,5 +9683,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmty4mlzr"
+  },
+  {
+    "questType": "anomaly300",
+    "quest": null,
+    "exStar": "EX5",
+    "rule": "ta",
+    "monsterId": "m38",
+    "weaponId": "gl",
+    "timeMs": 295980,
+    "author": "杞柳木昔_",
+    "date": "2026-07-05",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV19wTy6NECA",
+        "title": "【MHRS】逆推铳枪 Lv300千刃龙 TA4'55"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smty4o1ue"
   }
 ];
