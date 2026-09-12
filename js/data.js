cf0e53fa67640e1b9958abdf5eeb35404fa76fb1
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-12 17:48:38 +0800

    成绩更新（网页录入） 2026-09-12

--- js/data.js
+++ js/data.js
@@ -9851,5 +9851,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmty7b9ot"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX4",
+    "rule": "ta",
+    "monsterId": "m30",
+    "weaponId": "ig",
+    "timeMs": 592410,
+    "author": "治愈拽枪兵",
+    "date": "2026-08-31",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1Y4t865E4F",
+        "title": "虫棍雪鬼"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmty7d3d8"
   }
 ];
