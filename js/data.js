43bb47c450ce74b81517d3261e27128261a6c8be
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-12 16:27:20 +0800

    审核通过：投稿发布 2026-09-12

--- js/data.js
+++ js/data.js
@@ -9473,5 +9473,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "smty4gg3y"
+  },
+  {
+    "questType": "anomaly300",
+    "quest": null,
+    "exStar": "EX5",
+    "rule": "sanyou",
+    "monsterId": "m38",
+    "weaponId": "gs",
+    "timeMs": 234990,
+    "author": "开关_",
+    "date": "2025-04-01",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1aDZWYuEhw",
+        "title": "【MHRS/三台达成】流斩大剑 怪异探究：千刃龙 Lv300 3分54秒 三无规则"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smty4gkp2"
   }
 ];
