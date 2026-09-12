07540e91bf14d3d15ed00d6377e7d326e43c7fcc
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-12 13:23:04 +0800

    审核通过：投稿发布 2026-09-12

--- js/data.js
+++ js/data.js
@@ -8465,5 +8465,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "smtxxsr8i"
+  },
+  {
+    "questType": "anomaly300",
+    "quest": null,
+    "exStar": "EX3",
+    "rule": "sanyou",
+    "monsterId": "m21",
+    "weaponId": "sns",
+    "timeMs": 319990,
+    "author": "咩咩羊袭",
+    "date": "2026-04-07",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1w3DvB8EsW",
+        "title": "【MHRS/烂片】片手剑 Lv300毒妖鸟 5分19秒 三无规则"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "smtxxvm2b"
   }
 ];
