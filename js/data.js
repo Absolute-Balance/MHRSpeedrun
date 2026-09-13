5cd76c4c02cc087d1c5bac26a9cd1874b3dde89b
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-13 20:57:28 +0800

    成绩更新（网页录入） 2026-09-13

--- js/data.js
+++ js/data.js
@@ -8738,5 +8738,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmtz6wuxw"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX1",
+    "rule": "sanyou",
+    "monsterId": "m03",
+    "weaponId": "bow",
+    "timeMs": 165590,
+    "author": "加钱居士觉得很润",
+    "date": "2023-11-26",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1994y177ZA",
+        "title": "超特合集 一星 毒狗龙王2分45"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmtztjrip"
   }
 ];
