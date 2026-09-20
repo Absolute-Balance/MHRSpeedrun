d3dd7d90a224064850ebda7ecd5dcdbaea662037
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-20 13:33:46 +0800

    成绩更新（网页录入） 2026-09-20

--- js/data.js
+++ js/data.js
@@ -11493,27 +11493,6 @@ window.MHRS_RECORDS = [
   {
     "questType": "special",
     "quest": null,
-    "exStar": "EX2",
-    "rule": "sanyou",
-    "monsterId": "m11",
-    "weaponId": "ls",
-    "timeMs": 351060,
-    "author": "BFeather7",
-    "date": "2026-09-19",
-    "videos": [
-      {
-        "site": "bilibili",
-        "url": "https://www.bilibili.com/video/BV1tme86HExY",
-        "title": "【MHRS】太刀 特别探究：河童蛙 三无规则 5'51\"06"
-      }
-    ],
-    "platform": "steam",
-    "note": "",
-    "id": "rmu8ov1hb"
-  },
-  {
-    "questType": "special",
-    "quest": null,
     "exStar": "EX5",
     "rule": "sanyou",
     "monsterId": "m39",
