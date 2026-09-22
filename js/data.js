ccee0ade9009c2101f6edb5eb9ed1faf099a9a43
Absolute-Balance <54989735+Absolute-Balance@users.noreply.github.com>
2026-09-23 01:24:33 +0800

    成绩更新（网页录入） 2026-09-22

--- js/data.js
+++ js/data.js
@@ -12248,5 +12248,26 @@ window.MHRS_RECORDS = [
     "platform": "steam",
     "note": "",
     "id": "rmucxzocf"
+  },
+  {
+    "questType": "special",
+    "quest": null,
+    "exStar": "EX2",
+    "rule": "ta",
+    "monsterId": "m08",
+    "weaponId": "ig",
+    "timeMs": 391390,
+    "author": "治愈拽枪兵",
+    "date": "2026-09-22",
+    "videos": [
+      {
+        "site": "bilibili",
+        "url": "https://www.bilibili.com/video/BV1YEhH6bE5g",
+        "title": "一点特别探究（虫棍）"
+      }
+    ],
+    "platform": "steam",
+    "note": "",
+    "id": "rmucy1vz1"
   }
 ];
